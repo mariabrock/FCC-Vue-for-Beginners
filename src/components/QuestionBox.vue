c00b49d2e45fcbc8b5ebd105eda21b62aab3520d
Maria <mariabrock414@gmail.com>
2020-11-09 18:58:03 -0600

    can click on both buttons

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -12,26 +12,42 @@
           v-for="(answer, index) in answers" 
           :key="index"
           @click="selectAnswer(index)"
+          :class="[selectedIndex === index ? 'selected' : '']"
           >
           {{ answer }}
         </b-list-group-item>
       </b-list-group>
 
-      <b-button variant="success" href="#">Submit</b-button>
-      <b-button @click="next" variant="primary" href="#">Next</b-button>
+      <b-button 
+        variant="success"
+        @click="submitAnswer"
+        :disabled="selectedIndex === null"
+      >
+        Submit
+      </b-button>
+
+      <b-button @click="next" variant="primary">
+        Next
+      </b-button>
+
     </b-jumbotron>
   </div>
 </template>
 
 <script>
+import _ from 'lodash'
+
 export default {
   props: {
     currentQuestion: Object,
-    next: Function
+    next: Function,
+    increment: Function
   },
   data() {
     return {
-      selectedIndex: null
+      selectedIndex: null,
+      correctIndex: null,
+      shuffledAnswers: []
     }
   },
   computed: {
@@ -41,13 +57,32 @@ export default {
       return answers
     }
   },
+  watch: {
+    currentQuestion: {
+      immediate:true,
+      handler() {
+        this.selectedIndex = null
+        this.shuffleAnswers()
+      }
+    }
+  },
   methods: {
     selectAnswer(index) {
       this.selectedIndex= index
+    },
+    submitAnswer() {
+      let isCorrect = false
+
+      if (this.selectedIndex === this.correctIndex) {
+        isCorrect = true
+      }
+
+      this.increment(isCorrect)
+    },
+    shuffleAnswers() {
+      let answers = [...this.currentQuestion.incorrect_answers, this.currentQuestion.correct_answer]
+      this.shuffledAnswers = _.shuffle(answers)
     }
-  },
-  mounted() {
-    console.log(this.currentQuestion)
   }
 }
 </script>
@@ -56,7 +91,20 @@ export default {
   .list-group {
     margin-bottom: 15px;
   }
+  .list-group-item:hover {
+    background: #eeeeee;
+    cursor: pointer;
+  }
   .btn {
     margin: 0 5px;
+  }
+  .selected {
+    background-color: lightblue;
+  }
+  .correct {
+    background-color: lightgreen;
+  }
+  .incorrect {
+    background-color: red;
   }
 </style>
